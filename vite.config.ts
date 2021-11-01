45cc30a5a418326b2695889d58f7ea44e5613ba6
Sky12 <sun_is_you@163.com>
2021-11-01 10:52:17 +0800

    关于eslintplugin的插件配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+/*
+ * @Author: shan.zheng
+ * @Date: 2021-10-27 14:56:26
+ * @LastEditors: shan.zheng
+ * @LastEditTime: 2021-10-31 22:51:47
+ * @Description: vite项目配置
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import eslintPlugin from  'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue()
+    // eslintPlugin({
+    //   // 配置选项
+    //   cache: false, // 禁用eslint缓存
+    //   fix: false // 插件验证，建议关闭
+    // })
+  ]
 })
