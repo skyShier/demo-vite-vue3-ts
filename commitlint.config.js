f831e268ad45cac6e2045d29c901078e6c242eab
Sky12 <sun_is_you@163.com>
2021-11-02 20:28:56 +0800

    chore: 配置commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: shan.zheng
+ * @Date: 2021-11-02 11:11:50
+ * @LastEditors: shan.zheng
+ * @LastEditTime: 2021-11-02 20:14:00
+ * @Description: commit 提交规范配置文件
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+}
